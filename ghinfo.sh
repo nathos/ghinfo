47588fe80918b95cc53a0fd48acd31bf93084fca
Nathan Henderson <nathos@nathos.com>
2015-01-21 21:41:46 -0500

    Replace wonky array constructor with something a bit more robust. Fixes #11

--- ghinfo.sh
+++ ghinfo.sh
@@ -21,19 +21,21 @@ gh_request()
 
 api_request()
 {
-  IFS="" # Disable space as an array delimiter
   local api_request_result=$( gh_request "$1") # only make a single API request
   shift
 
   api_request_filtered=() # initialize/empty filtered results array
 
   # loop through all requested jq filter arguments
-  while [ "$1" ]; do
-    api_request_filtered+=( $( echo $api_request_result | jq -r "$1" )) # append to filtered result array
-    shift
+  for arg in $* ; do
+    _response=$( echo $api_request_result | jq -r "$arg" )
+    if [[ -n $_response ]] ; then
+      api_request_filtered+=("$_response") # append to filtered result array
+    # else
+    #   echo "No response for $arg"
+    fi
   done
 
-  unset IFS
 }
 
 jq_test()
@@ -88,34 +90,34 @@ repo_details()
     echo -n "="
   done
   echo -e "\n"
-  api_request "repos/$repo" '.name' '.owner.login' '.description' '.homepage' '.forks_count' '.stargazers_count' '.open_issues_count' '.created_at' '.updated_at' '.parent.name' '.parent.owner.login' '.clone_url'
+  api_request "repos/$repo" '.name' '.owner.login' '.description' '.forks_count' '.stargazers_count' '.open_issues_count' '.created_at' '.updated_at' '.parent.name' '.parent.owner.login' '.clone_url' '.homepage'
   echo -e " \033[1m${api_request_filtered[0]}\033[0m by ${api_request_filtered[1]}\n"
   local description=${api_request_filtered[2]}
   if [ "$description" != "null" ]; then
     echo " $description" | fmt
     echo ""
   fi
-  local homepage=${api_request_filtered[3]}
-  if [ $homepage != "null" ]; then
+  local homepage=${api_request_filtered[11]} # FIXME: dirty hack because GH returns an empty value instead of null
+  if [[ -n $homepage ]]; then
     echo " Homepage: $homepage"
   fi
-  echo -e "\n \033[35m$repo\033[0m has been forked \033[1;33m${api_request_filtered[4]}\033[0m times and starred \033[1;33m${api_request_filtered[5]}\033[0m times.\n"
+  echo -e "\n \033[35m$repo\033[0m has been forked \033[1;33m${api_request_filtered[3]}\033[0m times and starred \033[1;33m${api_request_filtered[4]}\033[0m times.\n"
   for i in $(seq 1 $name_length); do
     echo -n " "
   done
-  echo -e "  it has \033[1;33m${api_request_filtered[6]}\033[0m open issues.\n"
+  echo -e "  it has \033[1;33m${api_request_filtered[5]}\033[0m open issues.\n"
   for i in $(seq 1 $name_length); do
     echo -n " "
   done
-  local created_at=${api_request_filtered[7]}
-  local updated_at=${api_request_filtered[8]}
+  local created_at=${api_request_filtered[6]}
+  local updated_at=${api_request_filtered[7]}
   echo -e "  was created on \033[1;33m${created_at:0:10}\033[0m and last updated on \033[1;33m${updated_at:0:10}\033[0m."
-  local parent_name=${api_request_filtered[9]}
-  local parent_owner=${api_request_filtered[10]}
+  local parent_name=${api_request_filtered[8]}
+  local parent_owner=${api_request_filtered[9]}
   if [ $parent_name != "null" ]; then
     echo -e "\n\n \033[35m${api_request_filtered[0]}\033[0m was forked from \033[33m$parent_name\033[0m by \033[33m$parent_owner\033[0m"
   fi
-  echo -e "\n Clone URL: ${api_request_filtered[11]}"
+  echo -e "\n Clone URL: ${api_request_filtered[10]}"
 
   echo ""
   exit
